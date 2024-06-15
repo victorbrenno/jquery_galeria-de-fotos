09d46352c6dd5db8d664747c1b6618ad84f022cb
victor brenno <brksvito@gmail.com>
2024-06-15 19:12:00 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit',function(e) {
+        e.preventDefault();
+        const endereçoDaNovaImagem = $('#endereço-de-imagem-nova').val();
+        const novoItem = $(`<li style="display: none"><li/>`);
+        $(`<img src="${endereçoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${endereçoDaNovaImagem}" target="_blank" title="ver imagem em tamanho real" >
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000)
+        $('#endereco-imagem-nova').val('')
+    })
+})
